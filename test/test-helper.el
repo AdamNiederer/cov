35016ce5fa80118b0d53e1c629399789cf80e0dd
Thomas Fini Hansen <xen@xen.dk>
2018-04-07 22:46:54 +0200

    Add undercovers
    Eating our own dog-food.

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,3 +2,8 @@
 
 (defvar test-path
   (f-dirname (f-this-file)))
+
+(when (require 'undercover nil t)
+  ;; Track coverage, but don't send to coveralls (Travis will send it
+  ;; to Codecov).
+  (undercover "*.el" (:report-type :codecov)))
